ac3a893f27fb10ad13fe5cd4fef770741f44cc73
pjh0904 <pjh0904@jainwon.com>
2023-05-30 14:08:10 +0900

    init

---
+++ src/main/java/com/complete/multitenantschema/MultiTenantSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.complete.multitenantschema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantSchemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/complete/multitenantschema/MultiTenantSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complete.multitenantschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
